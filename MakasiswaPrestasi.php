fbccdb0e1112a8c0427ec4d65582c19669653c89
pandudangkeju <pandurendang@gmail.com>
2026-06-25 14:32:37 +0700

    tahap 5 mahasiswa Prestasi

--- MakasiswaPrestasi.php
+++ MakasiswaPrestasi.php
@@ -7,24 +7,24 @@ class MahasiswaPrestasi extends Mahasiswa {
     private string $namaInstansiBeasiswa;
     private float $minimalIpkSyarat;
 
+    // Constructor (Tetap dipertahankan untuk menangkap data awal)
     public function __construct(int $id_mahasiswa, string $nama_mahasiswa, string $nim, int $semester, float $tarifUKTnominal, string $namaInstansiBeasiswa, float $minimalIpkSyarat) {
         parent::__construct($id_mahasiswa, $nama_mahasiswa, $nim, $semester, $tarifUKTnominal);
         $this->namaInstansiBeasiswa = $namaInstansiBeasiswa;
         $this->minimalIpkSyarat = $minimalIpkSyarat;
     }
 
-    // Implementasi abstract method 1
+    // Overriding Tahap 5: Dapat potongan 75%, sehingga hanya bayar 25% dari UKT asli
     public function hitungTagihanSemester(): float {
-        // Jalur prestasi biasanya mendapat potongan / beasiswa parsial atau penuh
-        return $this->tarifUKTnominal; 
+        return $this->tarifUKTnominal * 0.25;
     }
 
-    // Implementasi abstract method 2
+    // Menampilkan spesifikasi akademik khusus Prestasi
     public function tampilkanSpesifikasiAkademik(): void {
         echo "Jalur: Prestasi | Instansi: {$this->namaInstansiBeasiswa} | Syarat Minimal IPK: {$this->minimalIpkSyarat}\n";
     }
 
-    // Method Query Spesifik: Mengambil data mahasiswa prestasi berdasarkan nama instansi pemberi beasiswa
+    // Method Query (Dari Tahap 4)
     public static function getByInstansiBeasiswa(PDO $db, string $instansi) {
         $sql = "SELECT id_mahasiswa, nama_mahasiswa, nim, nama_instansi_beasiswa, minimal_ipk_syarat 
                 FROM tabel_mahasiswa 
